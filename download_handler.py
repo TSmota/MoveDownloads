cd9a0e10c070e15fdb214e1bf30ef0fd3d235ffc
TSmota <41805986+TSmota@users.noreply.github.com>
2020-02-23 15:16:16 -0300

    Add files via upload
    Mover e organizar arquivos baixados no SSD para pasta Downloads no HD

---
+++ download_handler.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+from win10toast import ToastNotifier
+from watchdog.events import FileSystemEventHandler
+
+
+class DownloadHandler(FileSystemEventHandler):
+    def __init__(self, origin, destination):
+        self.folder = origin
+        self.destination_folder = destination
+        self.i = 0
+        self.running = False
+        self.notifier = ToastNotifier()
+    
+
+    def map_extension(self, ext):
+        word_exts = [".docx", ".doc"]
+        music_exts = [".mp3", ".iff", ".mpa", ".wav", ".m4a"]
+        exe_and_installers = [".exe", ".msi", ".jar", ".bat"]
+        photos = [".jpg", ".jpeg", ".png"]
+        videos = [".mp4", ".mkv", ".m4p", ".avi", ".wmv"]
+        code = [".c", ".java", ".class", ".cpp", ".cs", ".h", ".lua", ".m", ".py", ".sh", ".swift", ".vb", ".vcxproj"]
+
+        if ext in word_exts:
+            return "WORDS_DOCS"
+        elif ext in music_exts:
+            return "MUSICAS"
+        elif ext in exe_and_installers:
+            return "EXECUTAVEIS"
+        elif ext in photos:
+            return "FOTOS"
+        elif ext in videos:
+            return "VIDEOS"
+        elif ext in code:
+            return "DEV_CODES"
+        elif ext == ".pdf":
+            return "PDF_DOCS"
+        elif ext == ".txt":
+            return "TEXTOS"
+        elif ext == ".gif":
+            return "GIFS"
+        else:
+            return ext.upper()
+
+
+    def on_modified(self, event):
+        if self.running:
+            return
+        self.running = True
+        for filename in os.listdir(self.folder):
+            self.i = 0
+            base_name, extension = os.path.splitext(filename)
+
+            if extension and extension.upper() != '.CRDOWNLOAD': #arquivo de download em andamento
+                sub_folder = self.map_extension(extension.lower()) + "/"
+                if not os.path.exists(self.destination_folder + "/" + sub_folder):
+                    os.mkdir(self.destination_folder + "/" + sub_folder)
+                new_name = sub_folder + filename
+                file_exists = os.path.isfile(self.destination_folder + "/" + new_name)
+                while file_exists:
+                    self.i += 1
+                    new_name = "{} ({}){}".format(sub_folder + base_name, self.i, extension)
+                    file_exists = os.path.isfile("{}/{}".format(self.destination_folder, new_name))
+                source = self.folder + "/" + filename
+                new_destination = self.destination_folder + "/" + new_name
+                self.notifier.show_toast("Python", "Movendo arquivo {} para {}".format(filename, self.destination_folder +'/'+ sub_folder), duration=8)
+                shutil.move(source, new_destination)
+            elif not extension: #folders
+                new_destination = self.destination_folder + "/Folders/" + filename
+                folder_exists = os.path.exists(new_destination)
+                while folder_exists:
+                    self.i += 1
+                    new_name = "{} ({})".format(filename, self.i)
+                    new_destination = self.destination_folder + "/Folders/" + new_name
+                    folder_exists = os.path.exists(new_destination)
+                source = self.folder + "/" + filename
+                self.notifier.show_toast("Python", "Movendo pasta {} para {}".format(filename, self.destination_folder +'/Folders'), duration=8)
+                shutil.move(source, new_destination)
+        self.running = False
---
+++ move_downloads.py
@@ -0,0 +1,31 @@
+import os
+import time
+
+from watchdog.observers import Observer
+from win10toast import ToastNotifier
+
+from folders import folders_to_track
+from download_handler import DownloadHandler
+
+def main():
+    ToastNotifier().show_toast("Python - Mover arquivos em downloads", "Script para mover arquivos da pasta de download do SSD para HD iniciado")
+
+    folder_destination = "D:/Downloads"
+    observer = Observer()
+
+    for folder in folders_to_track:
+        event_handler = DownloadHandler(folder, folder_destination)
+        observer.schedule(event_handler, folder, recursive=True)
+        
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+if __name__ == "__main__":
+    main()
